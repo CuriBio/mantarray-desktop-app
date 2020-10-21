bce63f0610821c59e7973722a58edfe3deca0d56
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-21 22:13:47 +0000

    updated exe test to run in non-exe mode also

--- tests/test_exe.py
+++ tests/test_exe.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import subprocess
 
 from mantarray_desktop_app import CALIBRATED_STATE
@@ -10,17 +11,31 @@ from mantarray_desktop_app import wait_for_subprocesses_to_start
 import pytest
 import requests
 from stdlib_utils import confirm_port_in_use
+from stdlib_utils import get_current_file_abs_directory
+
+PATH_OF_CURRENT_FILE = get_current_file_abs_directory()
 
 
-@pytest.mark.only_exe
+@pytest.mark.slow
+# @pytest.mark.only_exe
 def test_exe_can_access_xem_script_and_firmware_folders():
-    # Tanner (6/18/20): These lines are necessary when testing outside of CodeBuild because no .exe exists
-    # import os
-    # from stdlib_utils import get_current_file_abs_directory
-    # subprocess.Popen(os.path.join(get_current_file_abs_directory(), "src", "entrypoint.py"))
-    subprocess.Popen("dist-python/mantarray-flask/mantarray-flask.exe")
+    # Eli (10/21/20): other parts of CI ensure that the EXE actually exists, so this can be run on the source file and the exe both to ensure we can catch test failures earlier
+    subprocess_args = [
+        os.path.join("dist-python", "mantarray-flask", "mantarray-flask.exe")
+    ]
+    if not os.path.isfile(subprocess_args[0]):
+        subprocess_args = [
+            "python3",
+            os.path.abspath(
+                os.path.join(
+                    get_current_file_abs_directory(), os.pardir, "src", "entrypoint.py"
+                )
+            ),
+        ]
+
+    subprocess.Popen(subprocess_args)
     port = get_server_port_number()
-    confirm_port_in_use(port, timeout=5)
+    confirm_port_in_use(port, timeout=10)
     wait_for_subprocesses_to_start()
     assert system_state_eventually_equals(CALIBRATION_NEEDED_STATE, 30) is True
 
