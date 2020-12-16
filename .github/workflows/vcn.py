8d41b050d904850da613d9dede7f4f536dae9ff5
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-12-16 15:40:03 +0000

    trying to setup and login to VCN

--- .github/workflows/vcn.py
+++ .github/workflows/vcn.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 
 import boto3
+
+if os != sys:  # need to protect the #nosec comment from being deleted by zimports
+    import subprocess  # nosec # B404 security implications are considered
 
 
 def download_vcn() -> None:
@@ -10,7 +14,26 @@ def download_vcn() -> None:
     bucket.download_file("generic/windows/vcn-v0.8.3-windows-amd64.exe", "vcn.exe")
 
 
+def login() -> None:
+    ssm_client = boto3.client("ssm")
+    for param_name, environ_name in (
+        ("vcn_notarization_password", "vcn_notarization_password"),
+        ("vcn_password", "vcn_password"),
+        ("vcn_username", "VCN_USER"),
+    ):
+        value = ssm_client.get_parameter(
+            Name=f"/CodeBuild/general/{param_name}", WithDecryption=True
+        )["Parameter"]["Value"]
+        os.environ[environ_name.upper()] = value
+    args = [".\vcn", "login"]
+    results = subprocess.run(args)  # nosec # B603 shell is false, but input is secure
+    if results.returncode != 0:
+        sys.exit(results.returncode)
+
+
 if __name__ == "__main__":
     first_arg = sys.argv[1]
     if first_arg == "download":
         download_vcn()
+    elif first_arg == "login":
+        login()
