7c67446c45760c8497b6f62211fb3df022acd98e
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-12-16 16:20:10 +0000

    updating vcn access to parameter store

--- .github/workflows/vcn.py
+++ .github/workflows/vcn.py
@@ -16,7 +16,7 @@ def download_vcn() -> None:
 
 
 def login() -> None:
-    my_config = Config(
+    my_config = Config(  # based on https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
         region_name="us-east-1",
         signature_version="v4",
         retries={"max_attempts": 10, "mode": "standard"},
@@ -28,9 +28,10 @@ def login() -> None:
         ("vcn_password", "vcn_password"),
         ("vcn_username", "VCN_USER"),
     ):
-        value = ssm_client.get_parameter(
-            Name=f"/CodeBuild/general/{param_name}", WithDecryption=True
-        )["Parameter"]["Value"]
+        # Eli (12/16/20): ran into odd permission errors trying to use `get_parameter`, so switching to `get_parameters`
+        value = ssm_client.get_parameters(
+            Names=[f"/CodeBuild/general/{param_name}"], WithDecryption=True
+        )["Parameters"][0]["Value"]
         os.environ[environ_name.upper()] = value
     args = [".\vcn", "login"]
     results = subprocess.run(args)  # nosec # B603 shell is false, but input is secure
