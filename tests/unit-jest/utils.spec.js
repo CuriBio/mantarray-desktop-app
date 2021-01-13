302d10c2818782e3cc617b97e4f993773b3482fd
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-01-13 18:32:58 +0000

    fixed some lint issues in utils.spec

--- tests/unit-jest/utils.spec.js
+++ tests/unit-jest/utils.spec.js
@@ -47,16 +47,16 @@ describe("Given a temporary directory is available", () => {
       beforeEach(() => {
         store = create_store({ file_path: tmp_dir_name });
       });
-      test("Then the log file directory argument is set to the folder containing the store", () => {
+      test("When the function is invoked, Then the log file directory argument is set to the folder containing the store", () => {
         const actual_args = generate_flask_command_line_args(store);
-        expect(actual_args).toEqual(
+        expect(actual_args).toStrictEqual(
           expect.arrayContaining([
             "--log-file-dir=" +
               path.join(path.dirname(store.path), "logs_flask"),
           ])
         );
       });
-      test("Then the --initial-base64-settings encoded settings argument is supplied only containing the recording directory if no ID exists in the store", () => {
+      test("When the function is invoked, Then the returned --initial-base64-settings encoded settings argument is supplied only containing the recording directory (since no ID exists in the store)", () => {
         const actual_args = generate_flask_command_line_args(store);
         const json_str = JSON.stringify({
           recording_directory: path.join(tmp_dir_name, "recordings"),
@@ -64,13 +64,13 @@ describe("Given a temporary directory is available", () => {
 
         const buf = Buffer.from(json_str, "utf8");
         const expected_encoded = url_safe_base64.encode(buf);
-        expect(actual_args).toEqual(
+        expect(actual_args).toStrictEqual(
           expect.arrayContaining([
             "--initial-base64-settings=" + expected_encoded,
           ])
         );
       });
-      test("Given the store has a customer and user account ID, Then the --initial-base64-settings encoded settings argument contains the current user account and customer account IDs from the store and the recording directory", () => {
+      test("Given the store has a customer and user account ID, When the function is invoked, Then the returned --initial-base64-settings encoded settings argument contains the current user account and customer account IDs from the store and the recording directory", () => {
         const id_list = store.get("customer_account_ids");
         id_list.push(generic_id_info);
         store.set("customer_account_ids", id_list);
@@ -84,13 +84,13 @@ describe("Given a temporary directory is available", () => {
 
         const buf = Buffer.from(json_str, "utf8");
         const expected_encoded = url_safe_base64.encode(buf);
-        expect(actual_args).toEqual(
+        expect(actual_args).toStrictEqual(
           expect.arrayContaining([
             "--initial-base64-settings=" + expected_encoded,
           ])
         );
       });
-      test("Then subfolders are created for logs and recordings", () => {
+      test("When the function is invoked, Then subfolders are created for logs and recordings", () => {
         generate_flask_command_line_args(store);
         expect(fs.existsSync(path.join(tmp_dir_name, "logs_flask"))).toBe(true);
         expect(fs.existsSync(path.join(tmp_dir_name, "recordings"))).toBe(true);
@@ -99,28 +99,27 @@ describe("Given a temporary directory is available", () => {
   });
 
   describe("create_store", () => {
-    describe("When called with a temporary absolute path", () => {
+    describe("Given create_store is called with a temporary absolute path", () => {
       beforeEach(() => {
         store = create_store({ file_path: tmp_dir_name });
       });
 
-      test("Then a real store gets returned that can set and get attributes", () => {
+      test("When a value is set in the store, Then it can be retrieved using get", () => {
         const the_key = "mykey";
         const expected_value = "myvalue";
         store.set(the_key, expected_value);
         const actual_value = store.get(the_key);
-        expect(actual_value).toEqual(expected_value);
-        const new_val = store.get("newkey");
+        expect(actual_value).toStrictEqual(expected_value);
       });
-      test("Then default empty list for customer account ids and account ID index and user ID index are set", () => {
+      test("When for customer account ids and account ID index and user ID index are accessed, Then they return the default value of an empty list", () => {
         let actual_value = store.get("customer_account_ids");
-        expect(actual_value).toEqual([]);
+        expect(actual_value).toStrictEqual([]);
         actual_value = store.get("active_customer_account_index");
-        expect(actual_value).toEqual(0);
+        expect(actual_value).toStrictEqual(0);
         actual_value = store.get("active_user_account_index");
-        expect(actual_value).toEqual(0);
+        expect(actual_value).toStrictEqual(0);
       });
-      test("When a customer account ID is added, then a new store instance can load it", () => {
+      test("When a customer account ID is added, Then a new store instance can load it", () => {
         const id_list = store.get("customer_account_ids");
         id_list.push(generic_id_info);
         store.set("customer_account_ids", id_list);
@@ -128,7 +127,7 @@ describe("Given a temporary directory is available", () => {
         const new_store = create_store({ file_path: tmp_dir_name });
         const actual_id_info = new_store.get("customer_account_ids")[0];
 
-        expect(actual_id_info).toEqual(generic_id_info);
+        expect(actual_id_info).toStrictEqual(generic_id_info);
       });
     });
   });
