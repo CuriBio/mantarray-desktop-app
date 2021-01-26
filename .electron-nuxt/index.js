a7a2980e8f37b7d8eaf69fa5bc50fe035cd8b512
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-01-26 18:07:33 +0000

    another attempt to suppress publishing

--- .electron-nuxt/index.js
+++ .electron-nuxt/index.js
@@ -35,10 +35,10 @@ let builder_config_path;
 builder_config_path = path.join(__dirname, "..", "electron-builder.yaml");
 
 const builder = new ElectronBuilder({
-  processArgv: {
-    config: builder_config_path,
-    publish: "never", // Eli (10/21/20): temporarily disable all publishing during transition to GitHub
-  },
+  processArgv: [
+    { config: builder_config_path },
+    { publish: "never" }, // Eli (10/21/20): temporarily disable all publishing during transition to GitHub
+  ],
 });
 
 const webpackConfig = Webpack.getBaseConfig({
