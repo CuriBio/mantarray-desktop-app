c5685926bc55fbc142d3a0ea9a72d2989a1b8c33
tanner <tanner@curibio.com>
2021-02-18 20:50:51 +0000

    added test for McCommunicationProcess.set_board_connection

--- tests/test_mc_comm.py
+++ tests/test_mc_comm.py
@@ -6,12 +6,11 @@ from mantarray_desktop_app import McCommunicationProcess
 from stdlib_utils import InfiniteProcess
 
 from .fixtures_mc_comm import fixture_four_board_mc_comm_process
+from .fixtures_mc_simulator import fixture_mantarray_mc_simulator
 from .helpers import confirm_queue_is_eventually_empty
 from .helpers import confirm_queue_is_eventually_of_size
 
-__fixtures__ = [
-    fixture_four_board_mc_comm_process,
-]
+__fixtures__ = [fixture_four_board_mc_comm_process, fixture_mantarray_mc_simulator]
 
 
 def test_McCommunicationProcess_super_is_called_during_init(mocker):
@@ -40,15 +39,30 @@ def test_McCommunicationProcess_hard_stop__clears_all_queues_and_returns_lists_o
     actual = mc_process.hard_stop()
     assert actual["fatal_error_reporter"] == [expected_error]
 
-    confirm_queue_is_eventually_empty(board_queues[0][0])
-    confirm_queue_is_eventually_empty(board_queues[0][2])
     confirm_queue_is_eventually_empty(board_queues[1][0])
     confirm_queue_is_eventually_empty(board_queues[2][0])
     confirm_queue_is_eventually_empty(board_queues[3][0])
+    confirm_queue_is_eventually_empty(board_queues[0][0])
+    confirm_queue_is_eventually_empty(board_queues[0][2])
 
-    assert actual["board_0"]["main_to_instrument_comm"] == [expected[0][0]]
-    assert expected[0][1] in actual["board_0"]["instrument_comm_to_main"]
-    assert actual["board_0"]["instrument_comm_to_file_writer"] == [expected[0][2]]
     assert actual["board_1"]["main_to_instrument_comm"] == [expected[1][0]]
     assert actual["board_2"]["main_to_instrument_comm"] == [expected[2][0]]
     assert actual["board_3"]["main_to_instrument_comm"] == [expected[3][0]]
+    assert actual["board_0"]["main_to_instrument_comm"] == [expected[0][0]]
+    assert expected[0][1] in actual["board_0"]["instrument_comm_to_main"]
+    assert actual["board_0"]["instrument_comm_to_file_writer"] == [expected[0][2]]
+
+
+def test_McCommunicationProcess_set_board_connection__sets_connect_to_mc_simulator_correctly(
+    four_board_mc_comm_process, mantarray_mc_simulator
+):
+    mc_process = four_board_mc_comm_process[0]
+    simulator = mantarray_mc_simulator[4]
+
+    mc_process.set_board_connection(1, simulator)
+    actual = mc_process.get_board_connections_list()
+
+    assert actual[0] is None
+    assert actual[1] is simulator
+    assert actual[2] is None
+    assert actual[3] is None
