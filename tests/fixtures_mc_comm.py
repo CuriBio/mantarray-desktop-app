d83793957a1d938e3489259155b61309b7afc640
tanner <tanner@curibio.com>
2021-02-24 19:20:27 +0000

    removed hard_stop from fixture_four_board_mc_comm_process

--- tests/fixtures_mc_comm.py
+++ tests/fixtures_mc_comm.py
@@ -9,8 +9,7 @@ from .fixtures import generate_board_and_error_queues
 
 @pytest.fixture(scope="function", name="four_board_mc_comm_process")
 def fixture_four_board_mc_comm_process():
+    # Tests using this fixture should be responsible for cleaning up the queues
     board_queues, error_queue = generate_board_and_error_queues(num_boards=4)
     p = McCommunicationProcess(board_queues, error_queue)
     yield p, board_queues, error_queue
-    # clean up queues to avoid broken pipe errors
-    p.hard_stop()
