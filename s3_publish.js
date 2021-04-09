a13be007cfdf460589c8dd2948b4d9c28ab0f490
Jason Person <jason.person@curibio.com>
2021-04-09 22:55:26 +0000

    updates

--- s3_publish.js
+++ s3_publish.js
@@ -14,9 +14,10 @@ const S3Publisher = require("app-builder-lib/out/publish/s3/s3Publisher")
 // const S3Publisher = require('electron-publisher-s3').default;
 const argv = require("yargs").argv;
 
-const buildDir = argv.buildDir || "./build/";
+const buildDir = argv.buildDir || "./dist/";
 const channel = argv.channel || "latest";
 const bucket = argv.bucket || "downloads.curibio.com";
+
 const publisherContext = {
   cancellationToken: new CancellationToken(),
   progress: new MultiProgress(),
@@ -32,9 +33,11 @@ const publisherInfo = {
 const publisher = new S3Publisher(publisherContext, publisherInfo);
 const upload = async () => {
   try {
-    await publisher.upload({ file: `${buildDir}/${argv.file}` });
-    await publisher.upload({ file: `${buildDir}/${argv.file}.blockmap` });
-    await publisher.upload({ file: `${buildDir}/${channel}.yml` });
+    await Promise.all([
+      publisher.upload({ file: `${buildDir}/${argv.file}` }),
+      publisher.upload({ file: `${buildDir}/${argv.file}.blockmap` }),
+      publisher.upload({ file: `${buildDir}/${channel}.yml` }),
+    ]);
   } catch (err) {
     console.error("Publisher Failed", err);
   }
