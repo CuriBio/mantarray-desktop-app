6811c359903e07c7a8da4d1dfc6b3575a449e53d
tanner <tanner@curibio.com>
2021-05-08 04:32:28 +0000

    added cython method stubs

--- src/mantarray_desktop_app/data_parsing_cy.pyx
+++ src/mantarray_desktop_app/data_parsing_cy.pyx
@@ -14,6 +14,9 @@ cdef map[int, map[int, int]] ADC_CH_TO_24_WELL_INDEX_C_MAP = dict(ADC_CH_TO_24_W
 cdef map[int, map[int, bint]] ADC_CH_TO_IS_REF_SENSOR_C_MAP = dict(ADC_CH_TO_IS_REF_SENSOR)
 cdef int RAW_TO_SIGNED_CONVERSION_VALUE_C_INT = RAW_TO_SIGNED_CONVERSION_VALUE
 
+
+# Beta 1 functions
+
 def parse_sensor_bytes(unsigned char[4] data_bytes) -> Tuple[bool, int, int]:
     """Parse the 32 bits of data related to a sensor reading.
 
@@ -51,7 +54,7 @@ cpdef (int, int, bint) parse_adc_metadata_byte(int metadata_byte):
     """Convert the metadata byte from the ADC to formatted data.
 
     Returns:
-        the ADC number and the channel number of that ADC
+        A tuple of the ADC number, the channel number of that ADC, and the value of the error bit
     """
     cdef int adc_num, adc_ch_num
     cdef bint error
@@ -66,3 +69,34 @@ cpdef int parse_little_endian_int24(unsigned char[3] data_bytes):
     """Convert 3 elements of a little-endian byte array into an int."""
     cdef int value = data_bytes[0] + (data_bytes[1] << 8) + (data_bytes[2] << 16)
     return value
+
+
+# Beta 2 functions
+
+def read_data_packets(read_func, int num_packets_to_read) -> Tuple[np.array, np.array, int, bytes]:
+    """Read the given number of data packets from the instrument.
+
+    If data stream is interrupted by packet that is not part of the data stream,
+    the packet bytes will be returned and the data arrays will not be full.
+
+    Args:
+        read_func: a pointer to the read function of the instrument's serial interface
+        num_packets_to_read: the desired number of packets to read
+
+    Returns:
+        A tuple of the array of parsed timestamps, the array of parsed data, the number of data packets read, and the bytes of an interrupting packet from the instrument (will be empty if one was not read)
+    """
+    pass
+
+
+cpdef (int, unsigned char, unsigned char) get_data_packet(char[:] data_buf):
+    """Read the next packet from the instrument. Load the packet body in the char buffer given
+
+    Args:
+        read_func: a pointer to the read function of the instrument's serial interface
+        data_buf: a buffer to load the packet body into
+
+    Returns:
+        The timestamp, module ID, and packet type of the data packet
+    """
+    pass
