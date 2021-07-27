46cf71393e1a174af129824ed6388ac50ab11a71
Tanner Peterson <tannermp23@hotmail.com>
2021-07-26 17:11:33 -0700

    fixed store init and moved it to layouts/default.vue which is only created once, so init only happens once

--- src/renderer/layouts/default.vue
+++ src/renderer/layouts/default.vue
@@ -67,6 +67,49 @@ export default {
       current_year: "2021", // new Date().getFullYear(),
     };
   },
+  created: function () {
+    // init pages here since this side bar is only created once
+
+    this.$store.commit("data/set_heatmap_values", {
+      "Twitch Force": { data: [...Array(24)].map((e) => Array(0)) },
+      "Twitch Frequency": { data: [...Array(24)].map((e) => Array(0)) },
+    });
+
+    this.$store.commit("waveform/set_y_axis_zoom_idx", 2);
+    this.$store.commit("waveform/set_y_axis_zoom_levels", [
+      { y_min: -100, y_max: 200 },
+      { y_min: 0, y_max: 150 },
+
+      { y_min: 50, y_max: 150 },
+      { y_min: 50, y_max: 150 },
+      { y_min: 50, y_max: 140 },
+      { y_min: 50, y_max: 130 },
+      { y_min: 55, y_max: 120 },
+      { y_min: 60, y_max: 120 },
+      { y_min: 60, y_max: 110 },
+      { y_min: 65, y_max: 110 },
+      { y_min: 70, y_max: 110 },
+      { y_min: 70, y_max: 105 },
+      { y_min: 75, y_max: 105 },
+      { y_min: 80, y_max: 105 },
+      { y_min: 85, y_max: 105 },
+      { y_min: 90, y_max: 105 },
+      { y_min: 94, y_max: 102 },
+      { y_min: 96, y_max: 100 },
+    ]);
+
+    this.$store.commit("waveform/set_x_axis_zoom_idx", 2);
+    this.$store.commit("waveform/set_x_axis_zoom_levels", [
+      { x_scale: 30 * 100000 },
+      { x_scale: 15 * 100000 },
+      { x_scale: 5 * 100000 },
+      { x_scale: 2 * 100000 },
+      { x_scale: 1 * 100000 },
+    ]);
+    this.$store.dispatch("flask/start_status_pinging");
+
+    console.log("Initial view has been rendered"); // allow-log
+  },
 };
 </script>
 
--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -82,50 +82,10 @@ export default {
     RecordingTime,
   },
   layout: "default",
-  created() {
-    // need to create this here so that errors won't happen if heatmap screen view is never selected
-    // TODO make sure these only get called once
-    this.$store.commit("data/set_heatmap_values", {
-      "Twitch Force": { data: Array(24).fill([]) },
-      "Twitch Frequency": { data: Array(24).fill([]) },
-    });
+  // created() {
 
-    this.$store.commit("waveform/set_y_axis_zoom_idx", 2);
-    this.$store.commit("waveform/set_y_axis_zoom_levels", [
-      { y_min: -100, y_max: 200 },
-      { y_min: 0, y_max: 150 },
-
-      { y_min: 50, y_max: 150 },
-      { y_min: 50, y_max: 150 },
-      { y_min: 50, y_max: 140 },
-      { y_min: 50, y_max: 130 },
-      { y_min: 55, y_max: 120 },
-      { y_min: 60, y_max: 120 },
-      { y_min: 60, y_max: 110 },
-      { y_min: 65, y_max: 110 },
-      { y_min: 70, y_max: 110 },
-      { y_min: 70, y_max: 105 },
-      { y_min: 75, y_max: 105 },
-      { y_min: 80, y_max: 105 },
-      { y_min: 85, y_max: 105 },
-      { y_min: 90, y_max: 105 },
-      { y_min: 94, y_max: 102 },
-      { y_min: 96, y_max: 100 },
-    ]);
-
-    this.$store.commit("waveform/set_x_axis_zoom_idx", 2);
-    this.$store.commit("waveform/set_x_axis_zoom_levels", [
-      { x_scale: 30 * 100000 },
-      { x_scale: 15 * 100000 },
-      { x_scale: 5 * 100000 },
-      { x_scale: 2 * 100000 },
-      { x_scale: 1 * 100000 },
-    ]);
-    this.$store.dispatch("flask/start_status_pinging");
-
-    console.log("Initial view has been rendered"); // allow-log
-  },
-  mounted() {},
+  // },
+  // mounted() {},
 };
 </script>
 
