60c316a6b8f88f167cde2c1025617bef556521d6
Tanner Peterson <tannermp23@hotmail.com>
2022-01-03 14:39:25 -0800

    ready to test autoupdate in windows

--- src/main/index.js
+++ src/main/index.js
@@ -4,6 +4,37 @@ const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
 const path = require("path");
 const features = require("./features.json");
+const now = new Date();
+const utc_month = (now.getUTCMonth() + 1).toString().padStart(2, "0"); // Eli (3/29/21) for some reason getUTCMonth returns a zero-based number, while everything else is a month, so adjusting here
+const filename_prefix = `mantarray_log__${now.getUTCFullYear()}_${utc_month}_${now
+  .getUTCDate()
+  .toString()
+  .padStart(2, "0")}_${now
+  .getUTCHours()
+  .toString()
+  .padStart(2, "0")}${now
+  .getUTCMinutes()
+  .toString()
+  .padStart(2, "0")}${now.getUTCSeconds().toString().padStart(2, "0")}_`;
+
+log.transports.file.resolvePath = (variables) => {
+  let filename;
+  switch (process.type) {
+    case "renderer":
+      filename = filename_prefix + "renderer";
+      break;
+    case "worker":
+      filename = filename_prefix + "worker";
+      break;
+    default:
+      filename = filename_prefix + "main";
+  }
+  filename = filename + ".txt";
+  return path.join(variables.libraryDefaultDir, "..", "logs_flask", filename);
+};
+console.log = log.log;
+console.error = log.error;
+
 const ci = require("ci-info");
 const fs = require("fs");
 const axios = require("axios");
@@ -50,8 +81,6 @@ const PY_SRC_FOLDER = "src"; // path to the python source
 const PY_MODULE = "entrypoint.py"; // the name of the main module
 const PY_EXE = "mantarray-flask"; // the name of the main module
 
-// const nodeConsole = require("console");
-// const myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 // When booting up (3/27/20), __dirname is equal to: win-unpacked\resources\app\dist\main
 const path_to_py_dist_folder = path.join(
   __dirname,
@@ -109,7 +138,6 @@ const start_python_subprocess = () => {
       "Launching compiled Python EXE at path: " +
         main_utils.redact_username_from_logs(script)
     );
-    // const subpy = require("child_process").execFile(script, command_line_args);
     require("child_process").execFile(script, command_line_args);
   } else {
     const PythonShell = require("python-shell").PythonShell; // Eli (4/15/20) experienced odd error where the compiled exe was not able to load package python-shell...but since it's only actually required in development, just moving it to here
@@ -132,9 +160,7 @@ const start_python_subprocess = () => {
         "' with options: " +
         JSON.stringify(redacted_options)
     );
-    // const pyshell = new PythonShell(py_file_name, options);
     new PythonShell(py_file_name, options);
-    // PythonShell.run(py_file_name,options)
   }
 };
 
@@ -157,12 +183,42 @@ ipcMain.on("save_customer_id", (e, customer_account) => {
   });
 });
 
-// let win_handler = null;
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  console.log("window-all-closed event being handled"); // allow-log
+  if (process.platform !== "darwin") app.quit();
+});
+
+const set_up_auto_updater = () => {
+  // autoUpdater.autoInstallOnAppQuit = false;
+
+  autoUpdater.setFeedURL({
+    provider: "s3",
+    bucket: "downloads.curibio.com",
+    path: "software",
+    channel: "alpha",
+    region: "us-east-1",
+    updaterCacheDirName: "mantarraycontroller-updater",
+  });
+  // set up handler for update-available event
+  autoUpdater.on("update-available", (update_info) => {
+    const new_version = update_info.releaseName;
+    console.log("$$$", new_version);
+    axios.post(
+      `http://localhost:${flask_port}/software_update_available?version=${new_version}`
+    );
+  });
+  // Check for updates. Will also automatically download the update as long as autoUpdater.autoDownload is true
+  autoUpdater.checkForUpdates();
+};
+
 app.on("ready", () => {
   if (!process.env.SPECTRON) {
     // disable on e2e test environment
     if (features.autoupdate) {
-      autoUpdater.checkForUpdatesAndNotify();
+      set_up_auto_updater();
     } else {
       console.log("Autoupdate feature disabled");
     }
@@ -176,7 +232,6 @@ app.on("will-quit", function (e) {
   // This is a good place to add tests ensuring the app is still
   // responsive and all windows are closed.
   console.log("will-quit event being handled"); // allow-log
-  // mainWindow = null;
 
   // Tanner (9/1/21): Need to prevent (default) app termination, wait for /shutdown response which confirms
   // that the backend is completely shutdown, then call app.exit() which terminates app immediately
