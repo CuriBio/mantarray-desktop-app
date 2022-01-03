2e4c9f8b09c1b917759171da2c93d3e9a1519aec
Tanner Peterson <tannermp23@hotmail.com>
2022-01-03 15:32:50 -0800

    added message to BE indicating the latest SW version

--- src/main/index.js
+++ src/main/index.js
@@ -10,12 +10,12 @@ const filename_prefix = `mantarray_log__${now.getUTCFullYear()}_${utc_month}_${n
   .getUTCDate()
   .toString()
   .padStart(2, "0")}_${now
-  .getUTCHours()
-  .toString()
-  .padStart(2, "0")}${now
-  .getUTCMinutes()
-  .toString()
-  .padStart(2, "0")}${now.getUTCSeconds().toString().padStart(2, "0")}_`;
+    .getUTCHours()
+    .toString()
+    .padStart(2, "0")}${now
+      .getUTCMinutes()
+      .toString()
+      .padStart(2, "0")}${now.getUTCSeconds().toString().padStart(2, "0")}_`;
 
 log.transports.file.resolvePath = (variables) => {
   let filename;
@@ -60,8 +60,8 @@ console.log = log.log;
 console.error = log.error;
 console.log(
   "Electron store at: '" +
-    main_utils.redact_username_from_logs(store.path) +
-    "'"
+  main_utils.redact_username_from_logs(store.path) +
+  "'"
 );
 
 global.__resources = undefined; // eslint-disable-line no-underscore-dangle
@@ -97,7 +97,7 @@ const isRunningInBundle = () => {
   console.log(
     // allow-log
     "To determine if running in bundle, checking the path " +
-      main_utils.redact_username_from_logs(path_to_py_dist_folder)
+    main_utils.redact_username_from_logs(path_to_py_dist_folder)
   );
   return fs.existsSync(path_to_py_dist_folder);
 };
@@ -113,10 +113,7 @@ const getPythonScriptPath = () => {
 };
 
 const start_python_subprocess = () => {
-  console.log(
-    // allow-log
-    "About to generate command line arguments to use when booting up server"
-  );
+  console.log("About to generate command line arguments to use when booting up server"); // allow-log
   const command_line_args = generate_flask_command_line_args(store);
   if (process.platform !== "win32") {
     // presumably running in a linux dev or CI environment
@@ -136,7 +133,7 @@ const start_python_subprocess = () => {
     console.log(
       // allow-log
       "Launching compiled Python EXE at path: " +
-        main_utils.redact_username_from_logs(script)
+      main_utils.redact_username_from_logs(script)
     );
     require("child_process").execFile(script, command_line_args);
   } else {
@@ -156,9 +153,9 @@ const start_python_subprocess = () => {
     console.log(
       // allow-log
       "Launching Python interpreter to run script '" +
-        py_file_name +
-        "' with options: " +
-        JSON.stringify(redacted_options)
+      py_file_name +
+      "' with options: " +
+      JSON.stringify(options)
     );
     new PythonShell(py_file_name, options);
   }
@@ -195,18 +192,14 @@ const set_up_auto_updater = () => {
   // set up handler for the event in which an update is found
   autoUpdater.once("update-available", (update_info) => {
     const new_version = update_info.version;
-    axios.post(
-      `http://localhost:${flask_port}/latest_software_version?version=${new_version}`
-    );
+    axios.post(`http://localhost:${flask_port}/latest_software_version?version=${new_version}`);
     // remove listeners for update-not-available since this event occured instead
     autoUpdater.removeAllListeners("update-not-available");
   });
 
   // set up handler for the event in which an update is not found
   autoUpdater.once("update-not-available", () => {
-    axios.post(
-      `http://localhost:${flask_port}/latest_software_version?version=null`
-    );
+    axios.post(`http://localhost:${flask_port}/latest_software_version?version=null`);
     // remove listeners for update-available since this event occured instead
     autoUpdater.removeAllListeners("update-available");
   });
@@ -234,19 +227,15 @@ app.on("will-quit", function (e) {
   // responsive and all windows are closed.
   console.log("will-quit event being handled"); // allow-log
 
-  // TODO find some way to check if firmware updates were found but not installed. If this is the case, prevent automatic SW updating. O/W, `autoUpdater.autoInstallOnAppQuit = true;`
+  // TODO find some way to check if firmware updates were found but not installed. If this is the case, prevent automatic SW updating. O/W, probably need use set up an event handler on ipcMain to set `autoUpdater.autoInstallOnAppQuit = true;`
   autoUpdater.autoInstallOnAppQuit = false;
 
   // Tanner (9/1/21): Need to prevent (default) app termination, wait for /shutdown response which confirms
   // that the backend is completely shutdown, then call app.exit() which terminates app immediately
   axios
-    .get(
-      `http://localhost:${flask_port}/shutdown?called_through_app_will_quit=true`
-    )
+    .get(`http://localhost:${flask_port}/shutdown?called_through_app_will_quit=true`)
     .then((response) => {
-      console.log(
-        `Shutdown response: ${response.status} ${response.statusText}`
-      ); // allow-log
+      console.log(`Shutdown response: ${response.status} ${response.statusText}`); // allow-log
       app.exit();
     })
     .catch((response) => {
