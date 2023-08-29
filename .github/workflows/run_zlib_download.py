e64846f03c24f1dbd8cb9398c0b29875644d9808
Tanner Peterson <tannermp23@hotmail.com>
2023-08-29 10:23:42 -0700

    fix zlib download

--- .github/workflows/run_zlib_download.py
+++ .github/workflows/run_zlib_download.py
@@ -9,11 +9,11 @@ import requests
 controller_folder_path = os.path.join("src", "mantarray_desktop_app")
 zlib_folder_path = os.path.join(controller_folder_path, "zlib")
 
-r = requests.get("https://www.zlib.net/zlib1213.zip", allow_redirects=True)
+r = requests.get("https://www.zlib.net/zlib13.zip", allow_redirects=True)
 with open(zlib_folder_path + ".zip", "wb") as zlib_zip:
     zlib_zip.write(r.content)
 
 with zipfile.ZipFile(zlib_folder_path + ".zip", "r") as zip_ref:
     zip_ref.extractall(controller_folder_path)
 
-os.rename(f"{zlib_folder_path}-1.2.13", zlib_folder_path)
+os.rename(f"{zlib_folder_path}-1.3", zlib_folder_path)
