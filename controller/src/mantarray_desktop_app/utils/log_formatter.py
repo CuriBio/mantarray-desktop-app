9a3e517acaf7cff778d7bb8e5c4be80449936305
Tanner Peterson <tannermp23@hotmail.com>
2023-09-18 13:42:31 -0700

    catch errors in log filter

--- controller/src/mantarray_desktop_app/utils/log_formatter.py
+++ controller/src/mantarray_desktop_app/utils/log_formatter.py
@@ -15,23 +15,26 @@ class SensitiveFormatter(logging.Formatter):
 
     @staticmethod
     def _filter(log_msg: str) -> Any:
-        if "/system_status" in log_msg and re.search(r"HTTP\S* 200 ", log_msg):
-            return None
-        elif "/set_mantarray_nickname" in log_msg:
-            return re.sub(
-                r"(.*set_mantarray_nickname\?nickname=)(.*)( HTTP.*)",
-                lambda match_obj: match_obj[1] + get_redacted_string(len(match_obj[2])) + match_obj[3],
-                log_msg,
-            )
-        else:
-            for route in ("/login", "/update_settings"):
-                if route in log_msg:
-                    return re.sub(
-                        rf"(.*{route}\?)(.*)( HTTP.*)",
-                        lambda match_obj: match_obj[1] + get_redacted_string(4) + match_obj[3],
-                        log_msg,
-                    )
-        return log_msg
+        try:
+            if "/system_status" in log_msg and re.search(r"HTTP\S* 200 ", log_msg):
+                return None
+            elif "/set_mantarray_nickname" in log_msg:
+                return re.sub(
+                    r"(.*set_mantarray_nickname\?nickname=)(.*)( HTTP.*)",
+                    lambda match_obj: match_obj[1] + get_redacted_string(len(match_obj[2])) + match_obj[3],
+                    log_msg,
+                )
+            else:
+                for route in ("/login", "/update_settings"):
+                    if route in log_msg:
+                        return re.sub(
+                            rf"(.*{route}\?)(.*)( HTTP.*)",
+                            lambda match_obj: match_obj[1] + get_redacted_string(4) + match_obj[3],
+                            log_msg,
+                        )
+            return log_msg
+        except Exception:
+            return f"ERROR LOGGING MSG: {log_msg}"
 
     def format(self, record: logging.LogRecord) -> Any:
         original = logging.Formatter.format(self, record)
