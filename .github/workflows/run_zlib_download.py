00968d4d48f55cb3b85cac2dfd89eaa8c53034ce
Tanner Peterson <tannermp23@hotmail.com>
2023-10-06 10:45:28 -0700

    new zlib download

--- .github/workflows/run_zlib_download.py
+++ .github/workflows/run_zlib_download.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
+import shutil
 import zipfile
 
-import requests
+import boto3
 
+s3_resource = boto3.resource("s3")
+bucket = s3_resource.Bucket("downloads.curibio.com")
 
 # assuming this is being run from controller/
 controller_folder_path = os.path.join("src", "mantarray_desktop_app")
+
+filename = "zlib-1.3.zip"
+zlib_zip_path = os.path.join(controller_folder_path, filename)
+bucket.download_file(f"software/{filename}", zlib_zip_path)
+
 zlib_folder_path = os.path.join(controller_folder_path, "zlib")
-
-r = requests.get("https://www.zlib.net/zlib13.zip", allow_redirects=True)
-with open(zlib_folder_path + ".zip", "wb") as zlib_zip:
-    zlib_zip.write(r.content)
-
-with zipfile.ZipFile(zlib_folder_path + ".zip", "r") as zip_ref:
+with zipfile.ZipFile(zlib_zip_path, "r") as zip_ref:
     zip_ref.extractall(controller_folder_path)
 
 os.rename(f"{zlib_folder_path}-1.3", zlib_folder_path)
+
+if "__MACOSX" in os.listdir(controller_folder_path):
+    shutil.rmtree(os.path.join(controller_folder_path, "__MACOSX"))
