b6425882f0f24d0d32b123be9339927645cd72e6
Tanner Peterson <tannermp23@hotmail.com>
2025-08-11 17:59:10 -0500

    fix tests

--- controller/src/mantarray_desktop_app/sub_processes/mc_comm.py
+++ controller/src/mantarray_desktop_app/sub_processes/mc_comm.py
@@ -444,11 +444,11 @@ class McCommunicationProcess(InstrumentCommProcess):
         self,
     ) -> Tuple[Union[MantarrayMcSimulator, serial.Serial, SerialDeviceFTDI], str]:
         # try to connect to instrument using FTDI driver
-        try:
+        try:  # pragma: no cover
             serial_conn = SerialDeviceFTDI()
             serial_conn.open()
             return serial_conn, "Connected to board using FTDI driver"
-        except Exception as e:
+        except Exception as e:  # pragma: no cover
             msg = f"Failed to connect using FTDI driver: {repr(e)}"
             put_log_message_into_queue(logging.INFO, msg, self._board_queues[0][1], self.get_logging_level())
 
